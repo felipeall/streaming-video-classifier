9b8267acc8082e3e007531464fd28fbbf04d7e97
Felipe Allegretti <felipeallh@gmail.com>
2023-03-09 12:05:52 -0300

    Add Video Producer local dev

--- apps/video-producer/src/main.py
+++ apps/video-producer/src/main.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import logging
+import os
 import time
 from dataclasses import dataclass
 from pathlib import Path
@@ -21,10 +22,23 @@ class KafkaProducer:
     videos_paths: list
 
     def __post_init__(self):
+        self._validate_args()
         self._create_producer()
+
+    def _validate_args(self):
+        self.bootstrap_server = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+        if self.bootstrap_server is None:
+            logging.critical("Missing `KAFKA_BOOTSTRAP_SERVERS` environment variable!")
+            raise SystemExit("Missing `KAFKA_BOOTSTRAP_SERVERS` environment variable!")
+
+        self.kafka_topic = os.getenv("KAFKA_TOPIC_NAME")
+        if self.kafka_topic is None:
+            logging.critical("Missing `KAFKA_TOPIC_NAME` environment variable!")
+            raise SystemExit("Missing `KAFKA_TOPIC_NAME` environment variable!")
 
     def _create_producer(self):
         try:
+            self.config_producer["bootstrap.servers"] = self.bootstrap_server
             logging.info(f"Connecting to Kafka server...")
             logging.info(f"Kafka Producer config: {self.config_producer}")
             self.producer = Producer(self.config_producer)
@@ -37,6 +51,7 @@ class KafkaProducer:
 
     def run(self):
         with concurrent.futures.ThreadPoolExecutor() as executor:
+            logging.info(f"Instantiating {str(executor._max_workers)} executor threads ...")
             executor.map(self.produce, self.videos_paths)
 
         self.producer.flush()
@@ -45,6 +60,13 @@ class KafkaProducer:
         logging.info(f"Processing video: {video_path} ...")
         video_name = str(Path(video_path).stem)
         video = cv2.VideoCapture(video_path)
+
+        if not video.isOpened():
+            logging.error(f"Error opening video: {video_path}")
+            video.release()
+            raise SystemExit(f"Error opening video: {video_path}")
+
+        fps = int(video.get(cv2.CAP_PROP_FPS))
         frame_no = 1
         try:
             while video.isOpened():
@@ -54,24 +76,32 @@ class KafkaProducer:
                     logging.exception(f"Invalid video file: {video_path}")
                     raise SystemExit(f"Invalid video file: {video_path}")
 
-                _, buffer = cv2.imencode(".jpg", frame)
+                if frame_no % fps == 0:  # extract frame every 1 second
 
-                self.producer.produce(
-                    topic="streaming-video-classifier",
-                    value=buffer.tobytes(),
-                    on_delivery=log_delivery_message,
-                    timestamp=frame_no,
-                    headers={"video_name": video_name},
-                )
+                    _, buffer = cv2.imencode(".jpg", frame)
+
+                    self.producer.produce(
+                        topic=self.kafka_topic,
+                        value=buffer.tobytes(),
+                        on_delivery=log_delivery_message,
+                        timestamp=frame_no,
+                        headers={"video_name": video_name},
+                    )
+
                 self.producer.poll(0)
                 time.sleep(0.2)
                 frame_no += 1
+
+        except KeyboardInterrupt:
+            print("Interrupted by the user! Exiting Kafka Producer...")
+            pass
 
         except Exception as e:
             logging.exception(f"Error producing message frames for video: {video_name} ({e})")
             raise
 
         finally:
+            logging.info(f"Releasing video: {video_name} ...")
             video.release()
 
         logging.info(f"Finished producing message frames for video: {video_name}")
